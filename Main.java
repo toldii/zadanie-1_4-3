857d6f0257560f5fa219bf5a2990b3bd11eef16e
toldii1 <100698275+toldii@users.noreply.github.com>
2022-03-25 23:43:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main{
+  public static void main(String[] args) {
+   Scanner scan = new Scanner(System.in);
+    System.out.println("podaj bok a");
+    double a = scan.nextInt();
+       System.out.println("podaj wysokość h");
+    double h = scan.nextInt();
+    double x = 0.5;
+    double wynik = x*a*h;
+    System.out.println("pole koła pierwszego wynosi: " +  wynik);
+
+  }
+}
